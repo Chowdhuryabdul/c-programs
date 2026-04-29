89d2e1a0c8b39df83c543a2edc4b76419137280d
Abdul Quaiyum <abdulquaiyum@Mac.home>
2026-04-29 18:07:25 +0200

    første

---
+++ conceptual-week-3/I_Smallest_Pair.c
@@ -0,0 +1,12 @@
+/* 1 ≤ i < j ≤ N
+what does it means?
+1 ≤ i	i is at least 1
+i < j	i is less than j
+j ≤ N	j is at most N
+
+if the value of n is 5
+if i is 1, then value of j will be 2, 3, 4, 5;
+if the value of i is 2, then value of j will be  3, 4, 5;
+if the value of i is 3, then value of j will be 4, 5;
+if the value of i is 4, then value of j will be  5;
+if the value of i can not be 5, then value of j will be 6; but our n value is 5 which means we have array of 5 elements.  */
