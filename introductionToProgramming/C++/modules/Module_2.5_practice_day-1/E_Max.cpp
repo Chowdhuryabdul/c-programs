ba8d4f4db9f54177193545d0af5b47caf77a8682
Abdul Quaiyum <abdulquaiyum@Mac.home>
2026-05-23 22:21:07 +0200

    last

--- introductionToProgramming/C++/modules/Module_2.5_practice_day-1/E_Max.cpp
+++ introductionToProgramming/C++/modules/Module_2.5_practice_day-1/E_Max.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<algorithm>
 
+#include<bits/stdc++.h>
+
 int main()
 {
     int n;
@@ -12,11 +14,26 @@ int main()
         std::cin>>a[i];
     }
 
-   int mx = a[0];
+    // int c++ programmming
+//    int mx = a[0];
+    int mx = INT_MIN;
     for (int i = 0; i < n; i++)
     {
         mx = std:: max(mx, a[i]);
     } 
+
+    // int c programing
+   /*  int mx = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+       
+    here we are keeping the value of a if it is bigger than max or if max and that value is similar the we keep that value in max.
+    mx = a[i];
+    mx = mx
+    if(a[i] > mx){
+        mx = a[i];
+       }
+    } */
 
     std::cout << mx <<std::endl;
     return 0;
--- introductionToProgramming/C++/modules/Module_2.5_practice_day-1/F_Reversing.cpp
+++ introductionToProgramming/C++/modules/Module_2.5_practice_day-1/F_Reversing.cpp
@@ -10,10 +10,34 @@ int main()
     {
         std::cin >> a[i]; 
     }
-    for (int i = 0; i < n/2; i++)
+    /* for (int i = 0; i < n/2; i++)
     {
         std::swap(a[i], a[n-1-i]);
-     }   
+     }  */  
+
+    /*  for (int i = 0; i < n; i++)
+     {
+        for (int j = n-1; j >= 0; j--)
+        {
+           std:: swap(a[i], a[j]);
+        }
+        
+        
+     } */
+
+
+     
+     int i = 0;
+     int j = n-1;
+     while(i < j){
+        // int tmp = a[i];
+        // a[i] = a[j];
+        // a[j] = tmp;
+        std::swap(a[i], a[j]);
+        i++;
+        j--;
+     }
+     
         
        for (int i = 0; i < n; i++)
     {
