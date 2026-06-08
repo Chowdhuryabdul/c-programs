7cfa6ec0b7bb34c90da0408802b0b87c24dd16eb
Abdul Quaiyum <abdulquaiyum@Mac.home>
2026-06-08 18:03:12 +0200

    last

--- introductionToProgramming/C++/modules/Module_5_string-class-object/string_modifer.cpp
+++ introductionToProgramming/C++/modules/Module_5_string-class-object/string_modifer.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    string s = "Hello World";
-    string s2 = "hi";
+    string s = "Hello world";
+    string s2 = "Hi";
     // s +=s2;
     s.append(s2); /* we can also do this with append function, here mention that one which will added */
     
@@ -13,6 +13,23 @@ int main()
 
     // to remove the last latter
     s.pop_back();
+
+    // to asing a new value
+    // s = "Gello";
+    s = s2;
+
+    // we can do it with the assign function
+    // s.assign(s2);
+    // s.assign("gello");
+
+    // to erase part of string - delete we can delete the whole but by erase we can delete part of it. we need to give that index, from where we want to start. if i want to delete a prticular part than i have to say from which index it will be started and how many it will be deleted. if you give one value than it will start from that value and will finish at the end.
+
+    s.erase(3, 2);
+
+    // to replace the part of string- here first one means where to start and 5 means how many will be deltted and than replace the world by Bangladesh
+    s.replace(6, 5, "Bangladesh");
+
+
     cout << s << endl;
 
     return 0;
